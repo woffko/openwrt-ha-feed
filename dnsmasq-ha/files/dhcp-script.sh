81506447b2b78b82894b903b3667bcad7fa1fa49
woffko <woffko@users.noreply.github.com>
2026-05-09 13:40:09 +0300

    lease-sync: fix DHCPv6 reflected lease loops

--- dnsmasq-ha/files/dhcp-script.sh
+++ dnsmasq-ha/files/dhcp-script.sh
@@ -6,14 +6,36 @@
 
 . /usr/share/libubox/jshn.sh
 
+is_dhcpv6_lease() {
+	[ -n "$DNSMASQ_IAID" ] && return 0
+
+	case "$1" in
+		*:*) return 0 ;;
+	esac
+
+	return 1
+}
+
+add_dnsmasq_lease_env() {
+	# Forward dnsmasq lease info (critical for lease-sync)
+	[ -n "$DNSMASQ_LEASE_EXPIRES" ] && json_add_string "" "DNSMASQ_LEASE_EXPIRES=$DNSMASQ_LEASE_EXPIRES"
+	[ -n "$DNSMASQ_TIME_REMAINING" ] && json_add_string "" "DNSMASQ_TIME_REMAINING=$DNSMASQ_TIME_REMAINING"
+	[ -n "$DNSMASQ_LEASE_LENGTH" ] && json_add_string "" "DNSMASQ_LEASE_LENGTH=$DNSMASQ_LEASE_LENGTH"
+	[ -n "$DNSMASQ_CLIENT_ID" ] && json_add_string "" "DNSMASQ_CLIENT_ID=$DNSMASQ_CLIENT_ID"
+	[ -n "$DNSMASQ_INTERFACE" ] && json_add_string "" "DNSMASQ_INTERFACE=$DNSMASQ_INTERFACE"
+	[ -n "$DNSMASQ_IAID" ] && json_add_string "" "DNSMASQ_IAID=$DNSMASQ_IAID"
+}
+
 json_init
 json_add_array env
 hotplugobj=""
 
 case "$1" in
 	add | del | old | arp-add | arp-del)
-		json_add_string "" "MACADDR=$2"
 		json_add_string "" "IPADDR=$3"
+		if ! is_dhcpv6_lease "$3"; then
+			json_add_string "" "MACADDR=$2"
+		fi
 	;;
 esac
 
@@ -21,30 +43,19 @@ case "$1" in
 	add)
 		json_add_string "" "ACTION=add"
 		json_add_string "" "HOSTNAME=$4"
-		# Forward dnsmasq lease info (critical for lease-sync)
-		[ -n "$DNSMASQ_LEASE_EXPIRES" ] && json_add_string "" "DNSMASQ_LEASE_EXPIRES=$DNSMASQ_LEASE_EXPIRES"
-		[ -n "$DNSMASQ_TIME_REMAINING" ] && json_add_string "" "DNSMASQ_TIME_REMAINING=$DNSMASQ_TIME_REMAINING"
-		[ -n "$DNSMASQ_LEASE_LENGTH" ] && json_add_string "" "DNSMASQ_LEASE_LENGTH=$DNSMASQ_LEASE_LENGTH"
-		[ -n "$DNSMASQ_CLIENT_ID" ] && json_add_string "" "DNSMASQ_CLIENT_ID=$DNSMASQ_CLIENT_ID"
-		[ -n "$DNSMASQ_INTERFACE" ] && json_add_string "" "DNSMASQ_INTERFACE=$DNSMASQ_INTERFACE"
-		[ -n "$DNSMASQ_IAID" ] && json_add_string "" "DNSMASQ_IAID=$DNSMASQ_IAID"
+		add_dnsmasq_lease_env
 		hotplugobj="dhcp"
 	;;
 	del)
 		json_add_string "" "ACTION=remove"
 		json_add_string "" "HOSTNAME=$4"
+		add_dnsmasq_lease_env
 		hotplugobj="dhcp"
 	;;
 	old)
 		json_add_string "" "ACTION=update"
 		json_add_string "" "HOSTNAME=$4"
-		# Forward dnsmasq lease info (critical for lease-sync)
-		[ -n "$DNSMASQ_LEASE_EXPIRES" ] && json_add_string "" "DNSMASQ_LEASE_EXPIRES=$DNSMASQ_LEASE_EXPIRES"
-		[ -n "$DNSMASQ_TIME_REMAINING" ] && json_add_string "" "DNSMASQ_TIME_REMAINING=$DNSMASQ_TIME_REMAINING"
-		[ -n "$DNSMASQ_LEASE_LENGTH" ] && json_add_string "" "DNSMASQ_LEASE_LENGTH=$DNSMASQ_LEASE_LENGTH"
-		[ -n "$DNSMASQ_CLIENT_ID" ] && json_add_string "" "DNSMASQ_CLIENT_ID=$DNSMASQ_CLIENT_ID"
-		[ -n "$DNSMASQ_INTERFACE" ] && json_add_string "" "DNSMASQ_INTERFACE=$DNSMASQ_INTERFACE"
-		[ -n "$DNSMASQ_IAID" ] && json_add_string "" "DNSMASQ_IAID=$DNSMASQ_IAID"
+		add_dnsmasq_lease_env
 		hotplugobj="dhcp"
 	;;
 	arp-add)
